e996c4d5b420a4af8f3c4b79f783213be5a99272
Blackicon <mattiaverdecchi@yahoo.it>
2025-07-02 17:21:29 +0200

    code cleanup

--- lib/utils.ts
+++ lib/utils.ts
@@ -11,11 +11,7 @@ import {
 } from "viem/chains";
 import { CheckoutPageStateType, UserAsset, PaginationState } from "./types";
 import { AlchemyRpcBaseUrls, PaymentPageState } from "./enums";
-import {
-  RoutesSupportedChainId,
-  RoutesSupportedStable,
-  stableAddresses,
-} from "@eco-foundation/routes-sdk";
+import { RoutesSupportedChainId } from "@eco-foundation/routes-sdk";
 import { Chain, createPublicClient, http } from "viem";
 import {
   INCREASE_L2_PROTOCOL_FEE,
